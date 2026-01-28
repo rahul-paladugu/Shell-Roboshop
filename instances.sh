7cb7baa293ac46a9f807e558d086e18c2ce4a445
rahul paladugu <rahulpaladugu96@gmail.com>
2026-01-28 13:25:58 +1300

    instances creation

---
+++ instances.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ami="ami-0220d79f3f480ecf5"
+sg="sg-03b441e0ba008f925"
+instances="mongodb, frontend, webserver"
+
+
+
+for instance in $instances
+do
+echo "Creating AWS INstances as requested"
+instance_id=$(aws ec2 run-instances --image-id $ami --instance-type t3.micro  --security-group-ids $sg  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"--query 'Instances[0].InstanceId' --output text)
+done
+echo $instance_id
--- variables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Select the Environment to deploy the new build"
-read Environment
-echo "We cannot implement changes in PROD"
