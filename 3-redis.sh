c703e581365a9e1ca56716dc46653ade60773777
rahul paladugu <rahulpaladugu96@gmail.com>
2026-02-03 12:01:44 +1300

    Redis-Configuration

--- 3-redis.sh
+++ 3-redis.sh
@@ -5,7 +5,10 @@ red="\e[31m"
 green="\e[32m"
 yellow="\e[33m"
 reset="\e[0m"
-
+id=$(id -u)
+if [ $id -ne 0 ]; then
+ echo -e "$red Please run the script using root access. $reset"
+fi
 #Catching Errors
 set -e
 trap 'echo -e "$red Error executing Line: $LINENO Command is $BASH_Command $reset"' ERR
