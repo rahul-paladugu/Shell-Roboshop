83585e2a89f5da63fb8fee050e6f35f7476e267c
rahul paladugu <rahulpaladugu96@gmail.com>
2026-02-03 13:26:39 +1300

    config

--- 5-cart.sh
+++ 5-cart.sh
@@ -28,7 +28,7 @@ dnf module enable nodejs:20 -y &>>$log
 echo -e "$yellow Installing Nodejs $reset"
 dnf install nodejs -y &>>$log
 echo -e "$yellow Creating System User $reset"
-if [ id -u roboshop ]; then
+if id -u roboshop ; then
  echo -e "$green user already exists. Hence skipping... $reset"
 else 
  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$log
