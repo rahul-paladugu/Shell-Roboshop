dbecdaca98a83cbc1fdb8f26d86d116d2712ab95
rahul paladugu <rahulpaladugu96@gmail.com>
2026-02-09 12:06:53 +1300

    Delete-old-logs

---
+++ Delete-old-logs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Adding Colours
+red="\e[31m"
+green="\e[32m"
+yellow="\e[33m"
+reset="\e[0m"
+files_path="/home/ec2-user/logs"
+logs=$(find $files_path -name "*.log" -mtime +14 -type f)
+root_user=$(id -u)
+#Check root access
+if [ $root_user -ne 0 ]; then
+  echo -e "${red}Please run the script using root access..${reset}"
+  exit 1
+fi
+
+while [ IFS=read -r log ];
+do
+ echo -e "Deleting the old logs..${yellow} $log ${reset}"
+ rm -rf $logs
+ echo -e "Deleted the log -${yellow} $log ${yellow}"
+done <<<$logs
